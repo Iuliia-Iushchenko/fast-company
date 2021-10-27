b3932fe44d86ffff7802599af2f7834a34d86988
Iuliia Iushchenko <julia.uschenko@yandex.ru>
2021-10-27 17:28:42 +1000

    added login page

--- src/app/components/layouts/login.jsx
+++ src/app/components/layouts/login.jsx
@@ -25,6 +25,16 @@ const Login = () => {
     password: {
       isRequired: {
         message: "Пароль обязателен для заполнения"
+      },
+      isCapitalSymbol: {
+        message: "Пароль должен содержать хотя бы одну заглавную букву"
+      },
+      isContainDigit: {
+        message: "Пароль должен содержать хотя бы одно число"
+      },
+      min: {
+        message: "Пароль должен состоять минимум из 8 символов",
+        value: 8
       }
     }
   };
@@ -38,6 +48,7 @@ const Login = () => {
     setErrors(errors);
     return Object.keys(errors).length === 0;
   };
+  const isValid = Object.keys(errors).length === 0;
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -46,24 +57,37 @@ const Login = () => {
     console.log(data);
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <TextField
-        label="Электронная почта"
-        name="email"
-        value={data.email}
-        onChange={handleChange}
-        error={errors.email}
-      />
-      <TextField
-        label="Пароль"
-        type="password"
-        name="password"
-        value={data.password}
-        onChange={handleChange}
-        error={errors.password}
-      />
-      <button type="submit">Submit</button>
-    </form>
+    <div className="container mt-5">
+      <div className="row">
+        <div className="col-md-6 offset-md-3 shadow p-4">
+          <h3 className="mb-4">Login</h3>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              label="Электронная почта"
+              name="email"
+              value={data.email}
+              onChange={handleChange}
+              error={errors.email}
+            />
+            <TextField
+              label="Пароль"
+              type="password"
+              name="password"
+              value={data.password}
+              onChange={handleChange}
+              error={errors.password}
+            />
+            <button
+              type="submit"
+              disabled={!isValid}
+              className="btn btn-primary w-100 mx-auto"
+            >
+              Submit
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
   );
 };
 
